a3bc01a31a6b2c8b25af70f2265c04e1b5e3793e
Yuji Fujiwara <y_fujiwr@dhsya151.o.aist.go.jp>
2019-10-08 13:30:06 +0900

    閾値を設定できるようにした

--- clone/calculate_alltype_precision.py
+++ clone/calculate_alltype_precision.py
@@ -2,12 +2,18 @@ import pandas as pd
 from pathlib import Path
 import re
 import sys
+from sklearn.metrics import precision_recall_fscore_support,roc_curve,roc_auc_score
+import os
+import matplotlib.pyplot as plt
+
+threshold = 0.15
+
 def read_csv(filename):
     dataframe = pd.read_csv(filename)
     def extractNum(func):
         return int(re.sub("\\D", "", func))
     def extractScore(func):
-        return float(func[1:-2])
+        return float(func[1:-1])
     dataframe["trues"] = dataframe["trues"].apply(extractNum)
     dataframe["predicts"] = dataframe["predicts"].apply(extractNum)
     dataframe["scores"] = dataframe["scores"].apply(extractScore)
@@ -21,10 +27,21 @@ for i in range(1,len(file_list)):
     dataForAppend = read_csv(file_list[i])
     dataForAppend = dataForAppend[dataForAppend["trues"]==1]
     data = data.append(dataForAppend)
-TruePositive = len(data[(data["trues"]==1) & (data["predicts"]==1)])
-FalsePositive = len(data[(data["trues"]==0) & (data["predicts"]==1)])
-FalseNegative = len(data[(data["trues"]==1) & (data["predicts"]==0)])
-TrueNegative = len(data[(data["trues"]==0) & (data["predicts"]==0)])
+TruePositive = len(data[(data["trues"]==1) & (data["scores"]>=threshold)])
+FalsePositive = len(data[(data["trues"]==0) & (data["scores"]>=threshold)])
+FalseNegative = len(data[(data["trues"]==1) & (data["scores"]<threshold)])
+TrueNegative = len(data[(data["trues"]==0) & (data["scores"]<threshold)])
 precision = TruePositive / (TruePositive+FalsePositive)
 recall = TruePositive / (TruePositive+FalseNegative)
 print("p:{},r:{},f:{}".format(precision,recall,2*precision*recall/(precision+recall)))
+
+trues = data["trues"].values
+scores = data["scores"].values
+
+fpr,tpr,thresholds = roc_curve(trues,scores)
+print("ROC_score: {}".format(roc_auc_score(trues,scores)))
+plt.plot(fpr, tpr, marker='o')
+plt.xlabel('FPR: False positive rate')
+plt.ylabel('TPR: True positive rate')
+plt.grid()
+plt.show()
